8715926478fcb676260d0d0304e44488c50b9cc2
Yanghwa <kellerj87@hotmail.com>
2018-07-10 10:52:29 -0400

    Mongoose Part 3

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -6,34 +6,44 @@ const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
-
+    
     console.log('Connected correctly to server');
 
     Dishes.create({
-        name: 'Uthapizza',
-        description: 'Test'
+        name: 'Uthappizza',
+        description: 'test'
     })
     .then((dish) => {
         console.log(dish);
-        
+
         return Dishes.findByIdAndUpdate(dish._id, {
-                $set: {
-                    description: 'Updated Test'
-                }
-            }, {
-                new: true
-            })
-            .exec();
+            $set: { description: 'Updated test'}
+        },{ 
+            new: true 
+        })
+        .exec();
     })
     .then((dish) => {
         console.log(dish);
-        return db.collection('dishes').drop();
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        return Dishes.collection.drop();
     })
     .then(() => {
-        return db.close();        
+        return db.connection.close();
     })
     .catch((err) => {
-
+        console.log(err);
     });
 
 });
