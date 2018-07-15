e4e2a5a9bc2b74879574980a924493bd7226a5c7
Yanghwa <kellerj87@hotmail.com>
2018-07-14 20:10:10 -0400

    HTTPS

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -24,6 +24,14 @@ connect.then((db) => {
 
 var app = express();
 
+app.all('*', (req, res, next) =>{
+  if(req.secure) {
+    return next();
+  } else {
+    res.redirect(307, 'https://' + req.hostname + ':' +app.get('secPort') + req.url);
+  }
+});
+
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 
