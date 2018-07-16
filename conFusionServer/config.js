27ff55aab240173059e224f920c0329c93a4d2d3
Yanghwa <kellerj87@hotmail.com>
2018-07-16 14:19:25 -0400

    Passport Facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-19483-59184-41811',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: 'Your Client App ID',
+        clientSecret: 'Your Client App Secret'
+    }
 }
